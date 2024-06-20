9ff67ed332ad90f61f8435c9b086e33d5065c467
agu_s <syquialuis@gmail.com>
2024-06-20 15:21:56 +0800

    Create SignUpForm component
    This will handle the inputs

---
+++ vue-everify-project/src/components/SignUpForm.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+        return {
+            name: ''
+        }
+    }
+}
+</script>
+<template>
+    <form>
+        <input type="text" required placeholder="First Name">
+        <input type="text" placeholder="Middle Name (optional)">
+        <input type="text" required placeholder="Last Name">
+        <input type="text" placeholder="Suffix (optional)">
+        <input type="date">
+        <input type="number" required placeholder="Mobile Number">
+        <input type="email" required placeholder="Email Address">
+    </form>
+
+</template>
+<style>
+
+
+</style>
