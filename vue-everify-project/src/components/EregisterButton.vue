da08fb0badeac60e021c3531c8522dead8ff0b73
agu_s <syquialuis@gmail.com>
2024-06-20 15:21:42 +0800

    Created EregisterButton component

---
+++ vue-everify-project/src/components/EregisterButton.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'EregisterButton',
+    methods: {
+        onClick() {
+            console.log('click')
+        }
+    }
+}
+
+</script>
+
+<template>
+    <button @click="onClick()">
+        Register with PhilSys ID
+    </button>
+
+</template>
+
+<style>
+</style>
