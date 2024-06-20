da3ef85978948bf3ca6a1242e4bb3ab3619ab743
agu_s <syquialuis@gmail.com>
2024-06-20 15:22:12 +0800

    Updated App.vue to work with other components

--- vue-everify-project/src/App.vue
+++ vue-everify-project/src/App.vue
@@ -1,33 +1,21 @@
-<script setup>
+<script>
+import SignUpForm from './components/SignUpForm.vue'
+import EregisterButton from './components/EregisterButton.vue'
+
+export default {
+    name: 'App',
+    components: {
+        SignUpForm,
+        EregisterButton
+    }
+}
 
 </script>
 
 <template>
-    <header>
-        <h1>BPI</h1>
-        <img src="./assets/bpi-logo.svg" alt="BPI logo" width="150">
-    </header>
-    <body>
-        <button> Register with PhilSys ID </button>
-        <h4>First Name</h4>
-        <input type="text">
-        <h4>Middle Name (optional)</h4>
-        <input type="text">
-        <h4>Last Name</h4>
-        <input type="text">
-        <h4>Suffix (optional)</h4>
-        <input type="text"> <br><br>
-        <label for="birthday"><b>Date of birth</b><i> (DD/MM/YYYY)</i><br></label><input type="date" id="birthday">
-        <h4>Mobile Number</h4>
-        <input type="text">
-        <h4>Email Address</h4>
-        <input type="email"> <br><br>
-        <input type="submit" value="Register">
-    </body>
-    <footer>
-        <p align="center"> Established 2024.</p>
-    </footer>
-  
+    <EregisterButton />
+    <SignUpForm />
+
 </template>
 
 <style scoped>
