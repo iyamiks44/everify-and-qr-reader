b12f3c957c3d6945971efee7ba33eff8769e72f9
Agu Syquia <luis.syquia@student.ateneo.edu>
2024-06-21 13:11:45 +0800

    Updated App.vue to work with QrReader.vue

--- vue-everify-project/src/App.vue
+++ vue-everify-project/src/App.vue
@@ -1,21 +1,39 @@
 <script>
 import SignUpForm from './components/SignUpForm.vue'
-import EregisterButton from './components/EregisterButton.vue'
+// import EregisterButton from './components/EregisterButton.vue'
+import QrReader from './components/QrReader.vue';
 
 
 export default {
     name: 'App',
     components: {
         SignUpForm,
-        EregisterButton,
-    }
+        // EregisterButton,
+        QrReader,
+    },
+    methods: {
+        showQrReader() {
+            this.scanning = true;
+            this.$refs.QrReader.scanning = true;
+        },
+    },
+    data() {
+        return {
+            scanning: false,
+            details: [],
+
+        }
+    },
 }
 
 </script>
 
 <template>
     <img src="./assets/bpi-logo.svg"><br>
-    <EregisterButton /><br>
+    <button @click="showQrReader()">
+        Register with PhilSys ID
+    </button><br>
+    <div><QrReader ref="QrReader" :scanning="scanning" /></div><br>
     <SignUpForm />
     
 </template>
