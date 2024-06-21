96a52ca55533baca052480c2f260951eb80e82f7
Agu Syquia <luis.syquia@student.ateneo.edu>
2024-06-21 11:21:00 +0800

    Updated to include v-models

--- vue-everify-project/src/components/SignUpForm.vue
+++ vue-everify-project/src/components/SignUpForm.vue
@@ -2,20 +2,25 @@
 export default {
     data() {
         return {
-            name: ''
+            firstName: '',
+            middleName: '',
+            lastName: '',
+            suffix: '',
+            cellNo: '',
+            email: '',
         }
     }
 }
 </script>
 <template>
     <form>
-        <input type="text" required placeholder="First Name">
-        <input type="text" placeholder="Middle Name (optional)">
-        <input type="text" required placeholder="Last Name">
-        <input type="text" placeholder="Suffix (optional)">
+        <input type="text" required placeholder="First Name" v-model="firstName">
+        <input type="text" placeholder="Middle Name (optional)" v-model="middleName">
+        <input type="text" required placeholder="Last Name" v-model="lastName">
+        <input type="text" placeholder="Suffix (optional)" v-model="suffix">
         <input type="date">
-        <input type="number" required placeholder="Mobile Number">
-        <input type="email" required placeholder="Email Address">
+        <input type="number" required placeholder="Mobile Number" v-model="cellNo">
+        <input type="email" required placeholder="Email Address" v-model="email">
         <button>Register</button>
     </form>
 
