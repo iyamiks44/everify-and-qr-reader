fbec83c28840450ba5cd5ef7bd46f2496a8c62d8
Agu Syquia <syquialuis@gmail.com>
2024-06-25 10:42:05 +0800

    Cleaned QrReader.vue

--- vue-everify-project/src/components/QrReader.vue
+++ vue-everify-project/src/components/QrReader.vue
@@ -7,21 +7,7 @@
     </div>
     <!-- <p>{{ qrData }}</p> -->
   </template>
-  <!-- <script setup>
-  import { computed } from 'vue'
-  import { useRoute, useRouter } from 'vue-router'
-    const router = useRouter()
-    const route = useRoute()
-
-  const search = computed({
-    get() {
-      return route.query.search ?? ''
-    },
-    set(search) {
-      router.replace({ query: {search}})
-    }
-  })
-</script> -->
+  
   <script>
   export default {
     name: 'QrReader',
