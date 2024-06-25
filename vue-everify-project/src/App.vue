a4bac200bcf88ccd53da5c2e134639e1afffc66b
Agu Syquia <syquialuis@gmail.com>
2024-06-25 14:24:03 +0800

    Cleaned up the code
    removed comments and unnecessary code

--- vue-everify-project/src/App.vue
+++ vue-everify-project/src/App.vue
@@ -1,8 +1,6 @@
 <script>
 import SignUpForm from './components/SignUpForm.vue';
 import QrReader from './components/QrReader.vue';
-import { RouterView } from 'vue-router';
-import { storeKey } from 'vuex';
 
 
 export default {
@@ -14,20 +12,9 @@ export default {
     methods: {
         changeScanning() {
             this.$store.commit('changeScanning',true);
-            this.$refs.QrReader.scanning = true;
-            console.log('changed')
+            this.$refs.QrReader.scanning = !this.$refs.QrReader.scanning;
+            // console.log('changed')
         }
-        // showQrReader() {
-        //     this.scanning = true;
-        //     this.$refs.QrReader.scanning = true;
-        // },
-        // fillData(data) {
-        //     this.details = data
-        // },
-        // goToQrReader() {
-        //     console.log('goto')
-        //     this.$router.push('/qrreader')
-        // }
     },
     data() {
         return {
@@ -42,20 +29,12 @@ export default {
 </script>
 
 <template>
-    <p>
-    <strong>Current route path:</strong> {{ $route.fullPath }}
-  </p>
     <img src="./assets/bpi-logo.svg"><br>
     <div v-show="!this.$store.state.scanning"><button @click="changeScanning()">
         Register with PhilSys ID
     </button></div>
-    <!-- <div v-show="!this.$store.state.scanning"><button @click="goToQrReader()">Qr Reader</button></div> -->
-    
     <div><QrReader ref="QrReader" :scanning="scanning" /></div><br>
-    <!-- <div v-on:scanned="fillData($emit)"></div> -->
-    <div v-show="!this.$store.state.scanning"><SignUpForm :information="details"/></div>
-    <!-- <RouterView></RouterView> -->
-    
+    <div v-show="!this.$store.state.scanning"><SignUpForm :information="details"/></div>    
 </template>
 
 <style scoped>
