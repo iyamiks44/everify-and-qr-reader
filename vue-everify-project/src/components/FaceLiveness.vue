d54b494779da89e4dc664b87cca116ca3fe320a1
iyamiks44 <layla.villanueva@student.ateneo.edu>
2024-06-27 09:40:40 +0800

    Updated FaceLiveness.vue return output format

--- vue-everify-project/src/components/FaceLiveness.vue
+++ vue-everify-project/src/components/FaceLiveness.vue
@@ -1,13 +1,21 @@
 <script>
 export default {
     name: 'FaceLiveness',
+    data(){
+      return {
+        status: '',
+        sessionID: '',
+        photoURL: ''
+      }
+    },
     methods: {
         startLiveness() {
             eKYC().start({
           pubKey: "eyJpdiI6IlVGbkt3OHdXdCsxWFRDOWs0eDZYaHc9PSIsInZhbHVlIjoiWmpHVDJNcU9GTE1aTjdsMmd4ZzRjQT09IiwibWFjIjoiOWFiOTNlOGZlNTlkYTI2N2EzZTUyMTdhMWIyMDdmODI3N2ZiMWZkYzRjYzFhMDU4MDc2NGRmZmQ3YmVmODc4MyIsInRhZyI6IiJ9"
         }).then((data) => {
-          const el = document.getElementById('response');
-          el.textContent = JSON.stringify(data, undefined, 2);
+          this.status = data.status;
+          this.sessionID = data.result.session_id;
+          this.photoURL = data.result.photo_url;
         }).catch((err) => {
           console.log('error', err);
         });
@@ -33,7 +41,11 @@ export default {
     <button @click="startLiveness()">
         Start Face Liveness
     </button>
-    <pre id="response"></pre>
+    <div v-if="status">
+      <p id="copyID">Session ID: {{ sessionID }}</p>
+      <p id="copyURL">Photo URL: <a :href="photoURL" target="_blank"> {{ photoURL }} </a></p>
+      
+  </div>
 
 </template>
 
